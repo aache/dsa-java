795f700049b333030188827b32bd1e87bfd057b6
aaqibhamdule@yahoo.com <aaqibhamdule@yahoo.com>
2026-01-17 11:50:07 +0530

    Leetcode SetMatrixZeroes.java

---
+++ src/main/java/org/example/SetMatrixZeroes.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetMatrixZeroes {
+    public void setZeroes(int[][] matrix) {
+        Set<Integer> rowSet = new HashSet<>();
+        Set<Integer> colSet = new HashSet<>();
+
+        for(int i = 0 ; i < matrix.length ; i++){
+            for(int j= 0 ; j < matrix[0].length ; j++){
+                if(matrix[i][j]==0){
+                    rowSet.add(i);
+                    colSet.add(j);
+                }
+            }
+        }
+
+        for(int i = 0 ; i < matrix.length ; i++){
+            for(int j= 0 ; j < matrix[0].length ; j++){
+                if(rowSet.contains(i) || colSet.contains(j)){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}
