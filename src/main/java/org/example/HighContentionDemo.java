0da00f9ae6d97286c801118fe84420ca12706409
aaqibhamdule@yahoo.com <aaqibhamdule@yahoo.com>
2026-06-03 00:59:39 +0530

    High Contention Demo

---
+++ src/main/java/org/example/HighContentionDemo.java
@@ -0,0 +1,36 @@
+package org.example;
+
+public class HighContentionDemo {
+
+    private static long counter = 0;
+
+//    public static synchronized void increment() { // synchroniation causes contention
+//        counter++;
+//    }
+
+    public static void increment() { //gives varying output
+        counter++;
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        long start = System.currentTimeMillis();
+
+        Thread[] threads = new Thread[100];
+
+        for (int i = 0; i < threads.length; i++) {
+            threads[i] = new Thread(() -> {
+                for (int j = 0; j < 1_000_000; j++) {
+                    increment();
+                }
+            });
+        }
+
+        for (Thread t : threads) t.start();
+        for (Thread t : threads) t.join();
+
+        System.out.println("Counter = " + counter);
+        System.out.println("Time = " +
+                (System.currentTimeMillis() - start) + " ms");
+    }
+}
