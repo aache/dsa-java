cf1326beed6c6787d0e094e26ef822ba72ec04c5
aache <aaqibhamdule@yahoo.com>
2026-06-10 00:27:18 +0530

    DP Climb Stairs

--- src/main/java/org/example/DPClimbStairs.java
+++ src/main/java/org/example/DPClimbStairs.java
@@ -1,15 +1,27 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class DPClimbStairs {
 
-    public int climbStairs(int n) {
+    Map<Integer, Long> map = new HashMap<>();
+    public long climbStairs(int n) {
+        if (map.containsKey(n)) {
+            return map.get(n);
+        }
         if(n == 1) return 1;
         if(n == 2) return 2;
-        return climbStairs(n-1) + climbStairs(n-2);
+        long result = climbStairs(n-1) + climbStairs(n-2);
+        map.put(n, result);
+        return result;
     }
 
     public static void main(String[] args) {
-        System.out.println(new DPClimbStairs().climbStairs(6));
+        DPClimbStairs dp = new DPClimbStairs();
+
+        System.out.println(dp.climbStairs(1000));
+        System.out.println(dp.map);
     }
 
 }
